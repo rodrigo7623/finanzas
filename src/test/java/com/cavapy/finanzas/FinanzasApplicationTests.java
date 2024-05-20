b865d9cd90a155a9966b652f6bb8b101d74f0822
rodrigo7623 <rodrigo7623@gmail.com>
2024-05-20 10:26:53 -0400

    primer commit de la api de finanzas

---
+++ src/test/java/com/cavapy/finanzas/FinanzasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cavapy.finanzas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
